fe6c519963392c9373eae0e326c4668d98f12224
rich-hart <richhohart@gmail.com>
2017-07-16 20:53:17 -0400

    init

---
+++ accounts/tests.py
@@ -0,0 +1,123 @@
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User, Group
+
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+#from myproject.apps.core.models import Account
+from selenium import webdriver
+from django.test import LiveServerTestCase
+import shutil
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import urllib
+
+DRIVER = 'CHROME'
+class TestLogin(LiveServerTestCase):
+    serialized_rollback = True
+
+    def setUp(self):
+        super(LiveServerTestCase, self).setUp()
+        admin_user = User.objects.create_superuser(
+            'admin', 
+            'u@d.com', 
+            'password123',
+        )
+        admin_user.save()
+        if DRIVER == 'CHROME':
+            self.driver = webdriver.Chrome()
+        else:
+
+            try:
+                phantomjs_path = os.environ['phantomjs']
+            except KeyError:
+                phantomjs_path = shutil.which("phantomjs")
+            self.driver = webdriver.PhantomJS(executable_path=phantomjs_path)
+
+    def test_admin_login(self):
+        import ipdb; ipdb.set_trace()
+        driver = self.driver
+        driver.get(urllib.parse.urljoin(self.live_server_url,'admin'))
+        self.assertIn('/admin/login/?next=/admin/',driver.current_url)
+        username_field = driver.find_element_by_id('id_username')
+        username_field.send_keys('admin')
+        password_field = driver.find_element_by_id('id_password')
+        password_field.send_keys('password123')
+        login_button  = driver.find_element_by_xpath('//*[@id="login-form"]/div[3]/input'
+)       
+        login_button.click()
+
+#        login_button = driver.find_element_by_tag_name('input')
+#        login_button.click()
+#        self.assertEqual(
+#            driver.current_url, 
+#            'http://localhost:8001/admin/login/?next=/admin/'
+#        )
+#        username_field = driver.find_element_by_name("auth_key")
+#        username_field.send_keys('admin')
+#        password_field = driver.find_element_by_name("password")
+#        password_field.send_keys('password123')
+#        sign_in_button = driver.find_element_by_class_name("standard")
+#        sign_in_button.click()
+        
+
+
+class AccountTests(APITestCase):
+    def test_create_admin_account(self):
+        pass
+
+
+#        """
+#        Ensure we can create a new account object.
+#        """
+#        url = reverse('account-list')
+#        data = {'name': 'DabApps'}
+#        response = self.client.post(url, data, format='json')
+#        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#        self.assertEqual(Account.objects.count(), 1)
+#        self.assertEqual(Account.objects.get().name, 'DabApps')
+
+
+
+class AccountTestCase(TestCase):
+
+    def setUp(self):
+        
+        self.client = Client()
+
+    def test_admin(self):
+        admin = User.objects.create_user(
+            'admin', 
+            'admin@test.com', 
+            'pass'
+        )
+        admin.save()
+#        response =self.client.post(
+#            '/admin/',
+#            {
+#                'username': 'john', 
+#                'password': 'smith'
+#            }
+#        , follow=True)
+#        self.assertEqual(response.status_code,400)
+
+    def test_staff(self):
+        pass
+
+    def test_anonymous(self):
+        pass
+
+    def test_user(self):
+        pass
+
+    def test_contact_information(self):
+        pass
+    
+class TestOAuth2(TestCase):
+    def test_facebook(self):
+        pass
+
+
+

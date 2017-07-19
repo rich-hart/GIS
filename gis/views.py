72f3483b0ad5b1987c763bfab02da350cca4f5f1
rich-hart <richhohart@gmail.com>
2017-07-19 16:54:01 -0400

    temp

--- gis/views.py
+++ gis/views.py
@@ -1,19 +1,23 @@
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth.models import AnonymousUser
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'home.html')
+    import ipdb; ipdb.set_trace()
+    if request.user.is_anonymous:
+        data = {
+            'username': None
+        }
+    else:
+        data = {
+            'username': request.user.username
+        }
+
+    return render(request, 'home.html', data)
 
 def facebook(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
     return render(request, 'facebook.html')
 
 def demo(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
     return render(request, 'demo.html')
 

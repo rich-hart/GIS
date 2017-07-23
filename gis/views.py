c73c3214af81766168365c31e69be750d76b89be
rich-hart <richhohart@gmail.com>
2017-07-23 20:23:39 +0000

    template interface, loading markers not working

--- gis/views.py
+++ gis/views.py
@@ -11,28 +11,29 @@ def qr_code_validator(request,key):
      
     return redirect('/?tribble={}#profile'.format(key))
 def home(request):
+    data = {
+      'username': None,
+      'address': None,
+      'tribble': None,
+      'google_id': None,
+      'longitude': None,
+      'latitude': None,
+      'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+    }
 #    import ipdb; ipdb.set_trace()
     user = request.user
 #    import ipdb; ipdb.set_trace()
+
     if user.is_anonymous:
-        data = {
-            'username': None,
-            'address': '',
-            'tribble': None,
-        }
+        pass
     elif user.is_superuser:
-        data = {
-            'username': user.username,
-            'address': 'NA',
-            'tribble': None,
-        }
+        data['username'] = user.username
     else:
-        data = {
-            'username': user.username,
-            'address': user.profile.address.raw,
-            'tribble': request.GET.get('tribble'),
-        }
-    data.update({'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY})
+        data['username'] = user.username
+        data['address'] = user.profile.address.raw
+        data['google_id'] = user.profile.address.google_id
+        data['longitude'] = user.profile.address.longitude
+        data['latitude'] = user.profile.address.latitude
     return render(request, 'home.html', data)
 
 def facebook(request):
@@ -62,6 +63,9 @@ def profile_form(request):
 #    import ipdb; ipdb.set_trace()
     profile = Profile.objects.get(owner=request.user)
     profile.address.raw = request.POST.get('address')
+    profile.address.google_id = request.POST.get('google_id')
+    profile.address.longitude = request.POST.get('longitude')
+    profile.address.latitude = request.POST.get('latitude')
     profile.address.save()
     return redirect('/#profile')
 

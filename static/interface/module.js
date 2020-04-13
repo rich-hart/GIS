d72e5930396b162a70d11b06c224720116ac3ba8
Rich Hart <richard.hart@capco.com>
2020-04-12 20:34:25 -0400

    add profile_pic id

--- static/interface/module.js
+++ static/interface/module.js
@@ -8,7 +8,7 @@ var uiColors = ['bg-blue-1', 'bg-blue-2','bg-blue-3','bg-blue-4','bg-green-1','b
    
 //Template for the Bracket Element   
 var bracket = {type:'wrapper', class:'sdk bracket typeA', children:[
-		{type:'wrapper', class:'content'},
+		{type:'wrapper', class:'content',id:'profile_pic'},
 		{type:'elbow', version:'top-left', size:'small', color:LCARS.colorGen(uiColors), children:[{type:'bar'}], noEvent:true},	
 		{type:'elbow', version:'top-right', size:'small', color:LCARS.colorGen(uiColors), children:[{type:'bar'}], noEvent:true},	
 		{type:'elbow', version:'bottom-left', size:'small', color:LCARS.colorGen(uiColors), children:[{type:'bar'}], noEvent:true},	

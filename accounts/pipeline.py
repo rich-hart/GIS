326ebdf545bc2d5890f1004e1990cd0e6c6d4d28
Rich Hart <richard.hart@capco.com>
2020-04-16 22:25:19 +0000

    bug-fix

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -1,9 +1,9 @@
 from .models import Profile
-from .models import Address
+#from .models import Address
 def save_profile(backend, user, response, *args, **kwargs):
     (profile, created) = Profile.objects.get_or_create(owner=user)
-    if not profile.address:
-        address = Address()
-        address.save()
-        profile.address = address
+#    if not profile.address:
+#        address = Address()
+#        address.save()
+#        profile.address = address
     profile.save()

9356b6097ca87e15c391f323fde23c6755f5de2f
Rich Hart <richard.hart@capco.com>
2020-04-29 23:35:35 +0000

    WIP

--- gis/heroku.py
+++ gis/heroku.py
@@ -7,10 +7,10 @@ from .settings import *
 DEBUG = False
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['SOCIAL_AUTH_FACEBOOK_SECRET']
-SECRET_KEY = os.environ['SECRET_KEY']
-SOCIAL_AUTH_FACEBOOK_KEY = os.environ['SOCIAL_AUTH_FACEBOOK_KEY']
-GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
+#SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['SOCIAL_AUTH_FACEBOOK_SECRET']
+#SECRET_KEY = os.environ['SECRET_KEY']
+#SOCIAL_AUTH_FACEBOOK_KEY = os.environ['SOCIAL_AUTH_FACEBOOK_KEY']
+#GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
 
 django_heroku.settings(locals())
